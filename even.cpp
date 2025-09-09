ee64d130781c51313dd44f4ff54f61a9bc11baaa
Avantika verma <your-vavantika141@gmail.com>
2025-09-09 22:17:35 +0530

    calculator

---
+++ even.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+string numberToWord(int n) 
+{
+    switch (n) {
+        case 1: return "one";
+        case 2: return "two";
+        case 3: return "three";
+        case 4: return "four";
+        case 5: return "five";
+        case 6: return "six";
+        case 7: return "seven";
+        case 8: return "eight";
+        case 9: return "nine";
+        default: return "";
+    }
+}
+int main() 
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            cout << numberToWord(i) << endl;
+        } else {
+            if (i % 2 == 0)
+                cout << "even" << endl;
+            else
+                cout << "odd" << endl;
+        }
+    }
+    return 0;
+}
