fe1bb3a7c34c43aa0ad3818eedc156fbdc59faf3
Avantika verma <vavantika141@gmail.com>
2026-08-19 00:29:16 +0530

    brothers

---
+++ Competitive_programming/brothers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string first_name_p1, last_name_p1;
+     string first_name_p2, last_name_p2;
+     cin>>first_name_p1 >>last_name_p1;
+     cin>>first_name_p2>>last_name_p2;
+     if(last_name_p1 == last_name_p2){
+         cout<< "Brothers";
+     }
+    return 0;
+}
--- Competitive_programming/digit summation.cpp
+++ Competitive_programming/digit summation.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int m, n;
+    long long m, n; // mistake make it long long instead of int 
     cin>>m>>n;
     int c=m%10+ n%10;
-    cout<<c;
+    cout<<c<<endl;
 }
