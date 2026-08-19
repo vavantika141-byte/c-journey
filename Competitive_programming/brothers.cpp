6adf87f748b42ab0fcfa140db0034eebabf444c9
Avantika verma <vavantika141@gmail.com>
2026-08-19 21:56:08 +0530

    comparison

--- Competitive_programming/brothers.cpp
+++ Competitive_programming/brothers.cpp
@@ -6,7 +6,11 @@ int main(){
      cin>>first_name_p1 >>last_name_p1;
      cin>>first_name_p2>>last_name_p2;
      if(last_name_p1 == last_name_p2){
-         cout<< "Brothers";
+         cout<< "ARE Brothers";
      }
+     else{
+         cout<<"NOT";
+     }
+
     return 0;
 }
