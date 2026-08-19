aa7f1a740fa08d92efed2dd98fac2506604356b6
Avantika verma <vavantika141@gmail.com>
2026-08-19 22:38:15 +0530

    ali baba

--- Competitive_programming/alibaba.cpp
+++ Competitive_programming/alibaba.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b,c,d;
+    long long a,b,c,d;
     cin>>a>>b>>c>>d;
     if((a+b-c==d)|| (a+(b*c)==d)|| (a-b+c==d)|| (a-(b*c)==d) || ((a*b)+c==d) || ((a*b)-c==d)){
         cout<<"YES";
