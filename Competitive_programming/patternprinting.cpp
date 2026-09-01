dc958d109e7ca034265b6f9fb1426aa43da93ce2
Avantika verma <vavantika141@gmail.com>
2026-09-01 21:07:44 +0530

    divisors

---
+++ Competitive_programming/patternprinting.cpp
@@ -0,0 +1,59 @@
+// Right half pyramid
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"*";
+        }
+       cout<<endl;
+    }
+    return 0;
+}
+*/
+
+// Floyd's triangle
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+   
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<n; i++){
+        for(int j=1; j<=i; j++){
+          cout<<n++<<" ";
+        }
+        cout<<"\n";
+    }
+}
+    */
+
+// Inverted  Right triangle
+/*
+*****
+****
+***
+**
+*
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n; i>=1; i--){
+      for(int j=1; j<=i; j++){
+        cout<<"*"<<" ";
+      }
+      cout<<endl;
+    }
+}
