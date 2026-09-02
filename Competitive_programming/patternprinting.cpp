47ee3d07fa5f1aa872eb98088c0a3314ef02c0d6
Avantika verma <vavantika141@gmail.com>
2026-09-02 17:09:10 +0530

    patterns

--- Competitive_programming/patternprinting.cpp
+++ Competitive_programming/patternprinting.cpp
@@ -57,3 +57,20 @@ int main(){
       cout<<endl;
     }
 }
+// LEFT HALF PYRAMID
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+     
+        for(int j=0;j<n-i-1; j++){
+            cout<<" ";
+        }
+        for(int k=0; k<=i; k++){
+            cout<<"*";
+        }
+    }
+    return 0;
+}
