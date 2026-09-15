a94037da1d6e4a9090d9809da8fc6a8db0ec2572
Avantika verma <vavantika141@gmail.com>
2026-09-16 01:49:29 +0530

    life, universe and the environment

--- Competitive_programming/luv.cpp
+++ Competitive_programming/luv.cpp
@@ -1,7 +1,7 @@
 /*
-lecture-4
+lecture-2 and 3
 
-*/
+
 //#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -9,8 +9,37 @@ int main(){
     //char, int, float, double, bool
     //long int , long long int
     // %- returns remainder
-    // 
-    char c='A';
-    cout<<(int)c; //typecasting
-    cout<<"hello world";
+    // left to right(+,-,*, %, /)
+    // -10^9< int<10^9
+    
+        char c='A';
+        int max= INT_MAX;
+        int min= INT_MIN;
+        cout<<min;
+        cout<<max;
+         //c=1e24;
+        cout<<fixed<<setprecision(0)<<c<<'\n';
+        cout<<(int)c<<'\n'; //typecasting-method, o/p is 65 its ASCII value
+        cout<<"hello world"<<'\n';
+        int a=9;
+        int b=7;
+        cout<<(bool)(a==b);
+        // &&- returns true if both are true
+        // ||- returns true if any one is true
+
+}
+
+*/
+
+// Lecture-4- loops
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0; i<=10; cout<<i++<<endl){
+
+    }
+    // in third part of loop{for} we can write any generalisation doesn't matter what
+    //break- it stops 
+    //continue- skips the statement
+
 }
