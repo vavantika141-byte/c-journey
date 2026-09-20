4f337cb9fee978c0e3d50bbccb31d9c0310c6a17
Avantika verma <vavantika141@gmail.com>
2026-09-20 21:02:28 +0530

    passing the ball

---
+++ Competitive_programming/passingtheball.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        string s;
+        cin>>n>>s;
+        int pos = s.find('L');   
+        cout<<pos + 1<<'\n'; 
+    }
+    return 0;
+}
